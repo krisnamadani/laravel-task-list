3169b01665106d013c57ee1d4764cf6e3ad228e2
krisna <krisnamadani@gmail.com>
2024-07-11 21:03:55 +0700

    forms & csrf protection

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,11 +13,18 @@ Route::get('/tasks', function () {
   ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
   return view('show', [
     'task' => \App\Models\Task::findOrFail($id)
   ]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
